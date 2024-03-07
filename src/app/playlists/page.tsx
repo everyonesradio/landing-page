a2fbbbf4ba32b67a256e3ed1ad4dae50c8da090e
DOGHOUSETV <arubigeorge0@gmail.com>
2024-03-07 02:37:55 -0500

    Playlist page

---
+++ src/app/playlists/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MusicAPIs = () => {
+  return (
+    <div className="w-full items-center justify-center lg:flex flex-col lg:pw-24 m-10 mb-5 overlay">
+      <div className="text-center text-white">
+        <h1 className="text-2xl lg:text-3xl leading-normal font-bold text-gradient">PLAYLISTS</h1>
+      </div>
+    </div>
+  );
+};
+
+export default MusicAPIs;
