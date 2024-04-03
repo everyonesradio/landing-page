25f5d3cb7e4d08e3d0a02cc87fc2ab6a0dda58d4
miguel l <m_n_l@miguels-mbp.mynetworksettings.com>
2024-04-03 00:37:49 -0400

    fix responsive issues, filter grid

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,11 +2,14 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <div className="w-full items-center justify-center lg:flex flex-col lg:pw-24 m-10 mb-5 overlay">
+    <div className="w-full items-center justify-start lg:flex flex-col lg:pw-24 overlay">
       <div className="text-center text-white">
         <h1 className="text-2xl lg:text-3xl leading-normal font-bold text-gradient">PROJECTS</h1>
+        </div>
+        <p className="text-base mt-4 text-black font-bold p-1">
+          Coming soon...
+        </p>
       </div>
-    </div>
   );
 };
 
