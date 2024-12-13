e0fce482f6a2747229559f84962bed8b1bd32395
DOGHOUSETV <arubigeorge0@gmail.com>
2024-12-13 11:44:42 -0500

    Update home page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import WelcomeBanner from "@/components/welcome-banner";
 
 const Home = () => {
   return (
-    <main className='flex flex-col items-center justify-center font-mono m-24'>
+    <main className='flex flex-col items-center justify-center font-mono mx-24 my-8'>
       <WelcomeBanner />
     </main>
   );
